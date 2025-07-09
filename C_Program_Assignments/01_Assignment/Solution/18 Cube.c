4a82af22a68247fce02e60daf478864b6daaea28
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-09 13:27:44 +0530

    Cube_of_Number

---
+++ C_Program_Assignments/01_Assignment/Solution/18 Cube.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0;
+
+    printf("\n Hello");
+
+    printf("\n\n Enter number to create its square : ");
+    scanf("%d",&No);
+
+    printf("\n\n Square of %d is => %d",No,No*No*No);
+
+    printf("\n\n Thank You");
+
+    getch();
+    return 0;
+}
