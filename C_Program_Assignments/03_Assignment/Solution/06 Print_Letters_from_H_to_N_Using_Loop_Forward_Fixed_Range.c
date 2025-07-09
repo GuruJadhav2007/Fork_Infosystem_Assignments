f40c57734821d49aac02b255e8bf6e0e9aae6dc8
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-09 14:21:00 +0530

    Print_Letters_from_H_to_N_Using_Loop_Fixed_Range

---
+++ C_Program_Assignments/03_Assignment/Solution/06 Print_Letters_from_H_to_N_Using_Loop_Forward_Fixed_Range.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Rng = 'H';
+
+    for(Rng = 'H'; Rng <= 'N'; Rng++)
+    {
+
+        printf("\n %c",Rng);
+    }
+    getch();
+    return 0;
+}
