6d6e07bea53528ba2360428f3b8bb5edec799f91
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-09 14:12:19 +0530

    Print_Number_From_101_to_110_Fixed_Range

---
+++ C_Program_Assignments/03_Assignment/Solution/01 Use_Loop_to_Print_Number_from_101_to_110.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 101, i = 0;
+
+    while(No <= 110)
+    {
+        printf("\n\n %d ",No++);
+        i++;
+    }
+    getch();
+    return 0;
+}
