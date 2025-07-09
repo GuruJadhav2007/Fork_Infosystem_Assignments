f0b27ef51ada296da02679f5f6b9316e80297935
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-09 13:30:02 +0530

    Swapping_of_2_Numbers

---
+++ C_Program_Assignments/01_Assignment/Solution/20 Swapping_2_No.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0, No2 = 0, Temp = 0;
+
+
+    printf(" Enter 1st number : ");
+    scanf("%d",&No1);
+
+    printf("\n\n Enter 2nd number : ");
+    scanf("%d",&No2);
+
+    printf("\n\n Before Swapping => \n\n First Number is : %d \n\n Second Number is : %d \n\n",No1,No2);
+
+    Temp = No1;
+    No1 = No2;
+    No2 = Temp;
+
+    getch();
+
+    printf("\n\n After Swapping => \n\n First Number is : %d \n\n Second Number is : %d \n\n",No1,No2);
+
+
+    getch();
+    return 0;
+}
