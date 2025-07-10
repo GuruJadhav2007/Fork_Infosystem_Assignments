5f6654e1f0efab7c724b3a16c5ac6b8da02373b7
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-10 09:57:20 +0530

    Array With_Initialization_Display_All_Elements

---
+++ C_Program_Assignments/07_Assignment/Solution/02 Array With_Initialization_Display_All_Elements.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[20] = {83,28,47,46};
+
+    printf("\n\n Number is %d ",Arr[0]);
+    printf("\n\n Number is %d ",Arr[1]);
+    printf("\n\n Number is %d ",Arr[2]);
+    printf("\n\n Number is %d ",Arr[3]);
+
+    getch();
+    return 0;
+}
