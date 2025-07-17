41cc0c2e09ae9f916789967a92e3813b022d00cd
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-17 16:06:16 +0530

    Array_of_7_Elements_with_Null_Initialization_Accept_All_Elements_without_Loop_Display_Its

---
+++ C_Program_Assignments/07_Assignment/Solution/04 Array_of_7_Elements_with_Null_Initialization_Accept_All_Elements_without_Loop_Display_Its.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[7] = {};
+
+
+    printf("\n Value of Element is : ");
+    scanf("%d",&Arr[0]);
+    printf("\n Value of Element is : ");
+    scanf("%d",&Arr[1]);
+    printf("\n Value of Element is : ");
+    scanf("%d",&Arr[2]);
+    printf("\n Value of Element is : ");
+    scanf("%d",&Arr[3]);
+    printf("\n Value of Element is : ");
+    scanf("%d",&Arr[4]);
+    printf("\n Value of Element is : ");
+    scanf("%d",&Arr[5]);
+    printf("\n Value of Element is : ");
+    scanf("%d",&Arr[6]);
+
+    printf("\n\n Value of Element is : %d",Arr[0]);
+    printf("\n\n Value of Element is : %d",Arr[1]);
+    printf("\n\n Value of Element is : %d",Arr[2]);
+    printf("\n\n Value of Element is : %d",Arr[3]);
+    printf("\n\n Value of Element is : %d",Arr[4]);
+    printf("\n\n Value of Element is : %d",Arr[5]);
+    printf("\n\n Value of Element is : %d",Arr[6]);
+
+
+    getch();
+    return 0;
+}
