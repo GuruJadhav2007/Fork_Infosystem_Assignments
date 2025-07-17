f32f0fe4b21bd28ef4eecb8fe2f71137a5dedfe9
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-17 16:05:33 +0530

    Array_of_7_Elements_with_Null_Initialization_Display_All_Elements

---
+++ C_Program_Assignments/07_Assignment/Solution/03 Array_of_7_Elements_with_Null_Initialization_Display_All_Elements.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[7] = {};
+
+    printf("\n\n Value of Element is %d",Arr[0]);
+    printf("\n\n Value of Element is %d",Arr[1]);
+    printf("\n\n Value of Element is %d",Arr[2]);
+    printf("\n\n Value of Element is %d",Arr[3]);
+    printf("\n\n Value of Element is %d",Arr[4]);
+    printf("\n\n Value of Element is %d",Arr[5]);
+    printf("\n\n Value of Element is %d",Arr[6]);
+
+    getch();
+    return 0;
+}
