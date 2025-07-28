cda4544033c74ea42a8535ea386a1ac6afcd3162
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-28 12:07:33 +0530

    Character_To_ASCII_Value

---
+++ C_Program_Assignments/01_Assignment/Solution/08 Character_To_ASCII_Value.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch = '\0';
+
+    printf("\n Enter a character to get ASCII value: ");
+    ch = getche();
+
+
+    printf("\n ASCII value of letter %c is => %d .",ch,ch);
+
+    printf("\n ========================================== Thanks");
+
+    getch ();
+    return 0;
+}
