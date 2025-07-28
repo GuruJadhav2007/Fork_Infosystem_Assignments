8a8b7445a3da20674a210aba51783075637d1fac
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-28 12:08:41 +0530

    ASCII_Value_To_Character

---
+++ C_Program_Assignments/01_Assignment/Solution/09 ASCII_Value_To_Character.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0;
+
+    printf("\n Enter ASCII value toget its Corresponding character : ");
+    scanf("%d",&No);
+
+    printf("\n ASCII letter for value %d is = %c.",No,No);
+
+    print("\n ============================\n");
+
+    getch();
+    return 0;
+}
