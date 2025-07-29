a741376239e4e2b57088f6c1a2b1b0955af061de
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-29 11:13:44 +0530

    Define_Funnction_Print_Welcome_Message_Function_Dose_Not_Take_Parameter_Dose_Not_Return_Value

---
+++ C_Program_Assignments/08 Assignment/Solutions/01 Define_Funnction_Print_Welcome_Message_Function_Dose_Not_Take_Parameter_Dose_Not_Return_Value.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Message();
+
+int main()
+{
+    Message();
+
+    getch;
+    return 0;
+}
+
+void Message()
+{
+    printf("\n Wel - Come ");
+
+    getch();
+    return 1;
+}
