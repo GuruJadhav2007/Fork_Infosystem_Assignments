df349688e28e43fa19f6d6d1b299f5af1f7cb257
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2026-01-23 12:19:00 +0530

    Print_Letter_W_to_E_Fixed_Range

---
+++ C_Program_Assignments/03_Assignment/Solution/07 Print_Letter_from_W_to_E_Fix_Range.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Rng = 'W';
+
+    for(Rng = 'W'; Rng >= 'E'; Rng--)
+    {
+
+        printf("\n %c",Rng);
+    }
+    getch();
+    return 0;
+}
