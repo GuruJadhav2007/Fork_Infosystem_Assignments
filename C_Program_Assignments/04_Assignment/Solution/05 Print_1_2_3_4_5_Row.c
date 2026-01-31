070cef5fa096ecab0782347d498ad9b953e17a54
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2026-01-31 15:26:51 +0530

    Print_1_2_3_4_5_Row

---
+++ C_Program_Assignments/04_Assignment/Solution/05 Print_1_2_3_4_5_Row.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int i = 0, j = 0, R = 0, C = 0;
+
+   printf("\n Enter Row Number : ");
+   scanf("%d",&R);
+   printf("\n Enter Column Number : ");
+   scanf("%d",&C);
+
+   printf("\n\n");
+
+   for(i = 1; i <= R; i++)
+   {
+       for(j = 1; j <= C; j++)
+       {
+            printf(" %d ", i);
+       }
+       printf("\n");
+   }
+
+   getch();
+   return 0;
+}
